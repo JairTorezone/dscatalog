9ad7ebf10dc440d07152bfc71c6cb60a3cdea514
Jair Torezone <jairtorezone@gmail.com>
2024-01-28 19:38:57 -0300

    (Frontend) - PrivateRoute

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
-            <h1>Conheça o melhor catálogo de produtos</h1>
+            <h1>Conheça o melhor catálogo de produtos </h1>
             <p>
               Ajudaremos você a encontrar os melhores produtos disponíveis no
               mercado.
